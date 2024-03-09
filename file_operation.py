9112faa15146a5b365b474c90696c040e1ea00b6
Safrin <mylifeofjava@gmail.com>
2024-03-09 09:46:16 +0800

    file

--- file_operation.py
+++ file_operation.py
@@ -2,3 +2,8 @@ with open("example.txt","a") as file:
     for i in range(10):
         file.write(str(i))
         file.write("\n")
+
+
+with open("example.txt","r") as file:
+    for i in file.readline():
+        print(i)
